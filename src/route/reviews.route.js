170d558268ed5a198418f93a5edf2253491677e1
Gift <Giftibe62@gmail.com>
2023-08-16 23:22:27 +0100

    feat: added joi validations routes

--- src/route/reviews.route.js
+++ src/route/reviews.route.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
+const {
+  validate_Form_Inputs
+} = require('../validation/joi.validations')
 const reviewRouter = Router();
 
 const { createReview, getCompanyReviews, getAllReviews } =
   (reviewController = require("../controller/review.controller"));
 
-reviewRouter.post("/reviews/create/:companyId", createReview);
+reviewRouter.post("/reviews/create/:companyId", validate_Form_Inputs, createReview);
 reviewRouter.get("/reviews/reviews", getAllReviews);
 reviewRouter.get("/reviews/:id", getCompanyReviews);
 
--- src/route/user.route.js
+++ src/route/user.route.js
@@ -1,4 +1,9 @@
 const { Router } = require('express')
+const {
+    validate_Account_Creation_Inputs,
+    validate_Company_Login_Inputs,
+} = require('../validation/joi.validations')
+
 const userRouter = Router()
 
 const {
@@ -12,8 +17,8 @@ const {
     loggedOut
 } = userController = require('../controller/user.controller')
 
-userRouter.post('/user/register', signUp)
-userRouter.post('/user/login', loginUser)
+userRouter.post('/user/register', validate_Account_Creation_Inputs, signUp)
+userRouter.post('/user/login', validate_Company_Login_Inputs, loginUser)
 userRouter.post('/user/logout', loggedOut)
 userRouter.get('/user', fetchAllUsers)
 userRouter.get('/user/:id', findAUser)
